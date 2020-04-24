887b1351a8b34ff5ae283fe0971b1cf355e1188c
smv1999 <vaidhyanathan.sm@gmail.com>
2020-04-24 19:58:07 +0530

    shift operation changes

--- Strings/string_shift_operations.cpp
+++ Strings/string_shift_operations.cpp
@@ -9,27 +9,17 @@ Left and Right Shift Operations by a particular length c
 #include<string.h>
 using namespace std;
 
-string shiftLeft(char* s,int c)
+string shiftLeft(string s,int c)
 {
-    int len = strlen(s);
-    int k=0;
-    string a="";
-    for(int i=0;i<c;i++)
-    {
-        a+=s[k++];
-    }
-    std::string st = ((string)s).substr(k, len-1);
-    return st+a;
+   return s.substr(c) + s.substr(0,c);
 }
-string shiftRight(char* s,int c)
+string shiftRight(string s,int c)
 {
-    int len = strlen(s);
-    int k = len-1;
-    string a="";
-    for(int i=0;i<c;i++)
-    {
-        a+=s[k--];
-    }
-    std::string st = ((string)s).substr(0, k+1);
-    return  a+st;
+    return s.substr(c+1) + s.substr(0,c+1);
+}
+int main(int argc, char const *argv[])
+{
+    cout<<shiftRight("Hello",2);
+    cout<<shiftLeft("Hello",2);
+    return 0;
 }
