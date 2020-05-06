ab5f36e8f0a3a18cbef41704455328e00a8245d3
smv1999 <vaidhyanathan.sm@gmail.com>
2020-05-06 17:24:03 +0530

    added inner reduce pattern printing

--- General Questions/inner_reduce_pattern.cpp
+++ General Questions/inner_reduce_pattern.cpp
@@ -18,21 +18,49 @@ Output
 
 
 */
+#include <bits/stdc++.h> 
+using namespace std; 
 
-// Work Under Progress 
+#define max 100 
 
-#include<iostream>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-    int n;
+void print(int a[][max], int size) 
+{ 
+for (int i = 0; i < size; i++) { 
+	for (int j = 0; j < size; j++) { 
+	cout << a[i][j]; 
+	} 
+	cout << endl; 
+} 
+} 
+
+void innerPattern(int n) { 
+	
+int size = 2 * n - 1; 
+int front = 0; 
+int back = size - 1; 
+int a[max][max]; 
+while (n != 0) 
+{ 
+	for (int i = front; i <= back; i++) { 
+	for (int j = front; j <= back; j++) { 
+		if (i == front || i == back || 
+			j == front || j == back) 
+		a[i][j] = n; 
+	} 
+	} 
+	++front; 
+	--back; 
+	--n; 
+} 
+print(a, size); 
+} 
+
+int main() 
+{ 
+	int n; 
     cin>>n;
-    for(int i=0;i<2*n-1;i++)
-    {
-        for(int j=0;j<2*n-1;j++)
-        {
-           
-        }
-    }
-    return 0;
-}
+	innerPattern(n); 
+	
+return 0; 
+} 
+
