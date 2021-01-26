14391d7e64cf0542163e05e0b89f2522aef84b70
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-26 13:50:50 +0530

    Update heap_sort.cpp

--- Sorting Algorithms/heap_sort.cpp
+++ Sorting Algorithms/heap_sort.cpp
@@ -3,56 +3,90 @@ and place the maximum element at the end. We repeat the same process for the rem
 */
 
 // C++ program for implementation of Heap Sort
+
 #include <iostream>
  
 using namespace std;
  
-// To heapify a subtree rooted with node i which is
-// an index in arr[]. n is size of heap
-void heapify(int arr[], int n, int i)
+// A function to heapify the array.
+void MaxHeapify(int a[], int i, int n)
 {
-    int largest = i; // Initialize largest as root
-    int l = 2 * i + 1; // left = 2*i + 1
-    int r = 2 * i + 2; // right = 2*i + 2
+	int j, temp;
+	temp = a[i];
+	j = 2*i;
  
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
- 
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
- 
-    // If largest is not root
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
- 
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
-    }
+ 	while (j <= n)
+	{
+		if (j < n && a[j+1] > a[j])
+		j = j+1;
+		// Break if parent value is already greater than child value.
+		if (temp > a[j])
+			break;
+		// Switching value with the parent node if temp < a[j].
+		else if (temp <= a[j])
+		{
+			a[j/2] = a[j];
+			j = 2*j;
+		}
+	}
+	a[j/2] = temp;
+	return;
 }
- 
-// main function to do heap sort
-void heapSort(int arr[], int n)
+void HeapSort(int a[], int n)
 {
-    // Build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
- 
-    // One by one extract an element from heap
-    for (int i = n - 1; i > 0; i--) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
- 
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
+	int i, temp;
+	for (i = n; i >= 2; i--)
+	{
+		// Storing maximum value at the end.
+		temp = a[i];
+		a[i] = a[1];
+		a[1] = temp;
+		// Building max heap of remaining element.
+		MaxHeapify(a, 1, i - 1);
+	}
 }
- 
-/* A utility function to print array of size n */
-void printArray(int arr[], int n)
+void Build_MaxHeap(int a[], int n)
 {
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << "\n";
+	int i;
+	for(i = n/2; i >= 1; i--)
+		MaxHeapify(a, i, n);
 }
+int main()
+{
+	int n, i;
+	cout<<"\nEnter the number of data element to be sorted: ";
+	cin>>n;
+	n++;
+	int arr[n];
+	for(i = 1; i < n; i++)
+	{
+		cout<<"Enter element "<<i<<": ";
+		cin>>arr[i];
+	}
+	// Building max heap.
+	Build_MaxHeap(arr, n-1);
+	HeapSort(arr, n-1);
+ 
+	// Printing the sorted data.
+	cout<<"\nSorted Data ";
+ 
+	for (i = 1; i < n; i++)
+		cout<<"->"<<arr[i];
+ 
+	return 0;
+}
+/* Output
+Enter the number of data element to be sorted: 10
+Enter element 1: 9
+Enter element 2: 6
+Enter element 3: 4
+Enter element 4: 3
+Enter element 5: 8
+Enter element 6: 7
+Enter element 7: 5
+Enter element 8: 2
+Enter element 9: 0
+Enter element 10: 1
+ 
+Sorted Data ->0->1->2->3->4->5->6->7->8->9
+/*
