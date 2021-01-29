b8e6792455796a3f763f281f59f2376cf3f89390
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-29 12:15:59 +0530

    Update move all negative numbers.cpp

--- DSA 450 GFG/move all negative numbers.cpp
+++ DSA 450 GFG/move all negative numbers.cpp
@@ -1,3 +1,11 @@
+/* Move all negative numbers to beginning and positive to end with constant extra space
+An array contains both positive and negative numbers in random order. Rearrange the array elements so that all negative numbers appear before all positive numbers.
+Examples : 
+
+Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
+Output: -12 -13 -5 -7 -3 -6 11 6 5
+*/
+
 // A C++ program to put all negative
 // numbers before positive numbers
 #include <bits/stdc++.h>
