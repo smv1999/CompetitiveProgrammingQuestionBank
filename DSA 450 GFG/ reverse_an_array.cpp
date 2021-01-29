feb062b5033970d24251ccdf57ddab7f5481b357
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-01-29 12:12:58 +0530

    Update and rename reverse an array or string.cpp to  reverse_an_array.cpp

--- DSA 450 GFG/ reverse_an_array.cpp
+++ DSA 450 GFG/ reverse_an_array.cpp
@@ -1,3 +1,17 @@
+/* Write a program to reverse an array
+Given an array (or string), the task is to reverse the array/string.
+Examples : 
+ 
+
+Input  : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input :  arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+
+*/
+
+
 // Iterative C++ program to reverse an array
 
 #include<bits/stdc++.h>
