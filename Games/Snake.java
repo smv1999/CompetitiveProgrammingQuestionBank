90e750c278256261aeb2f965246d7e32b42e0940
Hridaya <hridayahoney@gmail.com>
2021-02-05 08:40:50 +0530

    Create Snake.java

---
+++ Games/Snake.java
@@ -0,0 +1,32 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
