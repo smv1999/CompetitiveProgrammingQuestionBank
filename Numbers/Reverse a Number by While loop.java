c40ed95090cc53d93a5efae7d8be411f87938f69
Hridaya <hridayahoney@gmail.com>
2021-02-05 08:56:22 +0530

    Create Reverse a Number by While loop.java

---
+++ Numbers/Reverse a Number by While loop.java
@@ -0,0 +1,15 @@
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int num = 1234, reversed = 0;
+
+        while(num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + reversed);
+    }
+}
