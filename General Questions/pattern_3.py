df8413fc7ee934d796bdbe66d46ec5c6f508cb33
SarthakKeshari <srthkkeshari84@gmail.com>
2021-04-06 14:48:20 +0530

    Added Python programs Part - 3

---
+++ General Questions/pattern_3.py
@@ -0,0 +1,19 @@
+'''
+PRINT THE FOLLOWING STAR PATTERN :
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+*************** 
+'''
+
+#Taking user input
+n = int(input())
+
+for i in range(n,0,-1):
+    print(' '*(i-1),end="")
+    print('*'*(n-(i-1)),end="")
+    print('*'*(n-i))
---
+++ General Questions/pattern_5.py
@@ -0,0 +1,21 @@
+'''
+PRINT THE FOLLOWING STAR PATTERN :
+*****************
+ ***************
+  *************
+   ***********
+    *********
+     *******
+      *****
+       ***
+        *
+'''
+
+
+#Taking user input
+n = int(input())
+
+for i in range(n,0,-1):
+    print(' '*(n-i),end="")
+    print('*'*(i),end="")
+    print('*'*(i-1))
