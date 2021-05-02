257f42846ba8626b7e48dfa48c93ebe70cb31b88
Chiro <sachinsinghroxx@gmail.com>
2021-05-02 12:02:09 +0530

    added palindrome and DLL reverse for linked list

---
+++ Data Structures/Linked Lists/check_if_linkedlist_is_a_palindrome.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution
+{
+public:
+    //Function to check whether the list is palindrome.
+    Node *reverseList(Node *head)
+    {
+        Node *curr = head;
+        Node *temp = NULL;
+        Node *prev = NULL;
+        while (curr)
+        {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+    bool isPalindrome(Node *head)
+    {
+        Node *slow = head;
+        Node *fast = head;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        slow = reverseList(slow);
+        fast = head;
+        while (slow)
+        {
+            if (slow->data != fast->data)
+            {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return true;
+        //Your code here
+    }
+};
+
+int main()
+{
+    int T, i, n, l, firstdata;
+    cin >> T;
+    while (T--)
+    {
+
+        struct Node *head = NULL, *tail = NULL;
+        cin >> n;
+        // taking first data of LL
+        cin >> firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for (i = 1; i < n; i++)
+        {
+            cin >> l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        Solution obj;
+        cout << obj.isPalindrome(head) << endl;
+    }
+    return 0;
+}
---
+++ Data Structures/Linked Lists/reverse_a_double_linkedlist.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+void displayList(Node *head)
+{
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+// main function for reversing the doubly linked list
+Node* reverseDLL(Node * head)
+{
+    Node* temp = NULL;
+    Node* curr = head;
+    while(curr){
+        temp = curr->prev;
+        curr->prev=curr->next;
+        curr->next = temp;
+        curr=curr->prev;
+    }
+    
+    if(temp){
+        head= temp->prev;
+    }
+    
+    return head;
+}
+
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    Node *head=NULL, *tail=NULL;
+        int x;
+	    cin>>x;
+	    head = newNode(x);
+	    tail = head;
+	    
+	    for(int i=0;i<n - 1;i++)
+	    {
+	        cin>>x;
+	        Node* temp=newNode(x);
+	        tail->next=temp;
+	        temp->prev= tail;
+	        tail = temp;
+	    }
+	    head=reverseDLL(head);
+	    
+	    
+	    if(verify(head))
+	    displayList(head);
+	    else
+	    cout<<"Your pointers are not correctly connected";
+ 
+	    cout<<endl;
+	}
+	return 0;
+}
+
