d7de999b7b62e1ef4c89c3c833d38ecf40aa9a95
Manisha <76945004+MannyP31@users.noreply.github.com>
2021-05-23 22:52:20 +0530

    Update Conditional_statement_switch.js

--- Hackerrank solutions/10 days of js/Conditional_statement_switch.js
+++ Hackerrank solutions/10 days of js/Conditional_statement_switch.js
@@ -1,1 +1,46 @@
+'use strict';
 
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    
+    switch(s.charAt(0))
+    {
+        case('a'||'e'||'i'||'o'||'u'):
+          return 'A'
+        case('b'||'c'||'d'||'f'||'g'):
+          return 'B'
+        case('h'||'j'||'k'||'l'||'m'):
+          return 'C'
+         case('z'||'n'||'p'||'q'||'r'||'s'||'t'||'v'||'w'||'x'||'y'):
+          return 'D'
+          
+    }
+}
+
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
