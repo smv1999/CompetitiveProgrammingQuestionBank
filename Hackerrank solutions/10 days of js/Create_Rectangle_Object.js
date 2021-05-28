b0b3005dbe52c643338f2080ffa6097a4fb33092
Manisha <76945004+MannyP31@users.noreply.github.com>
2021-05-28 12:26:11 +0530

    Create Create_Rectangle_Object.js

---
+++ Hackerrank solutions/10 days of js/Create_Rectangle_Object.js
@@ -0,0 +1,33 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a,b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2 * (a + b);
+    this.area = (a * b);
+    }
