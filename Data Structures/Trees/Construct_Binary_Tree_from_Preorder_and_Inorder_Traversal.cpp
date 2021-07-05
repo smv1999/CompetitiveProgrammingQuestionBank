f55925b034cdce7ba35d831b576f3ecc2ef6e907
paurush batish <paurushbatishfbd@gmail.com>
2021-07-05 20:05:10 +0530

    Update Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
    Question description added

--- Data Structures/Trees/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ Data Structures/Trees/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -1,3 +1,17 @@
+// Leetcode problem for Binary Tree
+// 105. Construct Binary Tree from Preorder and Inorder Traversal
+
+// Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+// Example 1:
+
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+// Example 2:
+
+// Input: preorder = [-1], inorder = [-1]
+// Output: [-1]
+----------------------------------------------------------------------------
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
