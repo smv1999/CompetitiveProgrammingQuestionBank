ddf7899c1fcbfaf3490e41294754142c2fdd87b5
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-07-07 00:01:29 +0530

    Fixes #222

--- Dynamic Programming/binomial_coefficient.cpp
+++ Dynamic Programming/binomial_coefficient.cpp
@@ -1,3 +1,13 @@
+// Write a function that takes two parameters n and k and returns the value of Binomial Coefficient C(n, k).
+//  For example, our function should return 6 for n = 4 and k = 2, and it should return 10 for n = 5 and k = 2.
+
+// The idea we have used here is inspired by the pascal triangle.
+/*
+                    1
+                 1     1
+               1  2  2  1
+             1   3  4  3  1  */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,7 +23,7 @@ int binomial(int n, int r)
     {
         for (int j = 1; j <= r; j++)
         {
-            if (i == j || j == 0)
+            if (i == j || j == 0) // Base Condition
                 dp[i][j] = 1;
             else
             { //  we have taken a mod, so that higher results can be calculted too.
