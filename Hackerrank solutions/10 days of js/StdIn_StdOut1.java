c486c6c6e0ef3a4bbb52ab7eada60c1c52775bd3
Manisha <76945004+MannyP31@users.noreply.github.com>
2021-07-08 13:47:09 +0530

    Added some hackerrank solutions on java

---
+++ Hackerrank solutions/10 days of js/StdIn_StdOut1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+while(sc.hasNextInt()){
+    int a=sc.nextInt();
+    System.out.println(a);
+}
+    }
+}
