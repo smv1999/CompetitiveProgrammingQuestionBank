a76cc04c7f8016b2752f7f7e923f1f39debf2330
Simran Kumari <60690997+simranquirky@users.noreply.github.com>
2021-07-11 22:21:03 +0530

    Create binary_search.java
    Binary search using java language

---
+++ Searching Algorithms/binary_search.java
@@ -0,0 +1,69 @@
+
+/*
+On comiling the program will give output as follows:
+---------------------------------------------------
+Enter number of elements:
+7
+Enter 7 integers
+4
+5
+66
+77
+8
+99
+0
+Enter the search value:
+77
+77 found at location 4.
+-----------------------------------------------
+*/
+
+
+
+
+
+
+import java.util.Scanner;
+class BinarySearch
+{
+   public static void main(String args[])
+   {
+      int counter, num, item, array[], first, last, middle;
+      //To capture user input
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter number of elements:");
+      num = input.nextInt(); 
+
+      //Creating array to store the all the numbers
+      array = new int[num];
+
+      System.out.println("Enter " + num + " integers");
+      //Loop to store each numbers in array
+      for (counter = 0; counter < num; counter++)
+          array[counter] = input.nextInt();
+
+      System.out.println("Enter the search value:");
+      item = input.nextInt();
+      first = 0;
+      last = num - 1;
+      middle = (first + last)/2;
+
+      while( first <= last )
+      {
+         if ( array[middle] < item )
+           first = middle + 1;
+         else if ( array[middle] == item )
+         {
+           System.out.println(item + " found at location " + (middle + 1) + ".");
+           break;
+         }
+         else
+         {
+             last = middle - 1;
+         }
+         middle = (first + last)/2;
+      }
+      if ( first > last )
+          System.out.println(item + " is not found.\n");
+   }
+}
