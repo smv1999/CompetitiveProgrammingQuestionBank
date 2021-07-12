3426f32daf43505092aaba2a7bb49fb63e3a0cfb
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-07-12 20:41:26 +0530

    Update Kadane's_Algorithm.py

--- Arrays/Kadane's_Algorithm.py
+++ Arrays/Kadane's_Algorithm.py
@@ -4,10 +4,10 @@ def kadaneAlgo(n,arr):
     sum_num = 0
     max_sum = 0
     for i in range(n):
-        sum_num += arr[i];
+        sum_num += arr[i];   #maximum ending here
         if arr[i] > sum_num:
             sum_num = arr[i]
-        if max_sum < sum_num:
+        if max_sum < sum_num:     #comparing maximum ending here , maximum so far
             max_sum = sum_num
     return max_sum
  
