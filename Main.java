b277883060d9a709f0f71fa22ab066068a51ee1a
Mayuri-cell <80411472+Mayuri-cell@users.noreply.github.com>
2021-07-13 17:28:54 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
+/* Title : To find the prime numbers present in the range . 
+   Description : User need to enter the input number till which prime present can be printed.
+*/
 import java.util.Scanner ;
 class Main 
 {
 public static void main(String[] ags)
 { Scanner read = new Scanner(System.in);
   int i, n;
-  System.out.println("Enter the number till which you wish to finad all the prime no.s : ");
-  n=read.nextInt();
+  System.out.println("Enter the number till which you wish to finad all the prime no.s : ");   
+  n=read.nextInt();                                                                              /* Prime number will be printed from 2 to n */
   System.out.println(" Prime numbers from 1 to "+n+ " are : ");
    for(i=2;i<=n;i++)
    { if((i==2)||(i==3)||(i==5)||(i==7)||(i==11)||(i==13)||(i==17))
