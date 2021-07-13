5f3e6d94f8627dc6a6ad2ca83b798966d1802cd9
aakankshabhende <aakanksha0407@gmail.com>
2021-07-13 11:02:22 +0530

    python-linked-list-all-operations

---
+++ Data Structures/Linked Lists/Singly Linked List/Single-linked-list-operations.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
+            itr = itr.next
+        print(llstr)
+
+    def length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+
+        return count
+
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.length():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count += 1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+
+if __name__ == '__main__':
+    node1 = LinkedList()
+    node1.insert_values(["python","C++","C","Java"])
+    node1.insert_at(1,"Javascript")
+    node1.remove_at(2)
+    node1.print()
+    node1.insert_values([45,7,12,567,99])
+    node1.insert_at_end(67)
+    node1.print()
