f7f98da208ca69d6f890d2193e5e6bc3bb1565bb
ishikasinha-d <69107978+ishikasinha-d@users.noreply.github.com>
2021-07-15 22:31:25 +0530

    Update integer_to_roman.cpp

--- Strings/integer_to_roman.cpp
+++ Strings/integer_to_roman.cpp
@@ -9,8 +9,14 @@ typedef long long int ll;
 string int_to_roman(ll n)
 {
    string str="";
+   // store all base values in vector in descending order
    vector <int > v ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+
+   // store roman numeral equivalent of each base value using map
    map <int, string> m ={{1000,"M"}, {900,"CM"},{500,"D"},{400,"CD"},{100,"C"},{90,"XC"},{50,"L"},{40,"XL"},{10,"X"},{9,"IX"},{5,"V"},{4,"IV"},{1,"I"}};
+
+   //now find the largest base value 
+   //dividing the number and repeat the symbol accordingly
   
    for(auto x: v)
    {
@@ -19,7 +25,7 @@ string int_to_roman(ll n)
          int i= n/x;
          n= n%x;
          while(i--)
-            str+= m[x];
+            str+= m[x];   //repeating the correspoding roman symbol
       }
       else
       return str;
