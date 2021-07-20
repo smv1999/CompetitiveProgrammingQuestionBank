10cb09ccae37c26e40bbac3d6949a1388abe54be
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2021-07-20 17:50:19 +0530

    Update SilverAndGold.java
    Added problem statement at start of file

--- SilverAndGold.java
+++ SilverAndGold.java
@@ -1,3 +1,27 @@
+/* ************
+Problem statement:
+SILVER AND GOLD
+
+There are N coins kept in a line. 
+Each coin has two sides - one is colored gold
+and the other silver. You can flip two adjacent
+coins any number of times. You need to make 
+the gold-colored side of every coin facing up.
+You are given the initial status of coins
+in a binary string s where 1 represents the 
+gold side facing up and 0 represents the silver
+side facing up. If it is possible to make the
+gold-colored side of every coin facing up,
+return Yes, otherwise return No
+
+Input:
+N = 8, s = "11001100"
+Output:
+Yes
+Explanation:
+Flipping 3rd and 4th coin together and 7th
+and 8th coin together will do the task.
+********* */
 public class SilverAndGold{
 	//Silver and Gold Problem	
 	public static String flipCoins(int N,String s) {
