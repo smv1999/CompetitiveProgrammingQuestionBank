93b685e11969c40902f11759231b561b6d68632a
Ayonija Kaushik <84201769+Ayonijakaushik19@users.noreply.github.com>
2021-07-21 18:38:33 +0530

    Create Merge Two Sorted Array.cpp

---
+++ Arrays/Merge Two Sorted Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) { 
+    // Optimal Solution
+    // Two pointer approach without any extra space
+    int p1 = m - 1, p2 = n - 1, curr=m+n-1;
+    for (int i = curr; i >= 0 && p2 >=0 ;i--) {  
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) 
+                nums1[i] = nums1[p1--]; 
+            else 
+                nums1[i] = nums2[p2--];
+        }  
+    }
+};
