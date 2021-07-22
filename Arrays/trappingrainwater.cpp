82bc2a87eb6d4955127b96e76e73145dc36ddaf0
vishvarana <85014366+vishvarana@users.noreply.github.com>
2021-07-22 15:26:46 +0530

    Update trappingrainwater.cpp

--- Arrays/trappingrainwater.cpp
+++ Arrays/trappingrainwater.cpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 int main(){
-    int arr[]={1,8,6,2,5,4,8,3,7};
+    int arr[]={1,8,6,2,5,4,8,3,7}; //array of diff heights
     int l=0,n=sizeof(arr)/sizeof(int),r=n-1;
     int max_area=0,h,a,b;
+    //2 pointers algorithm
 
-    while(l<r){
-        h=min(arr[l],arr[r]);
-        b=r-l;
+    while(l<r){ 
+        h=min(arr[l],arr[r]); //choosing the minimum of both heights -left and right side
+        b=r-l; //base = right height index - left 
         a=h*b;
-        max_area=max(a,max_area);
+        max_area=max(a,max_area); //idea to maximize the area to trap more water
         if(arr[l]<=arr[r]){
-            l++;
+            l++; //if right ride height is greater move left pointer to choose max of heights
         }
         else{
-            r--;
+            r--; //else move right pointer to choose next max height
         }
     }
     cout<<"Maximum water that can be trapped is : "<<max_area;
