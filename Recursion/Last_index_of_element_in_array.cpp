77d90ebc419f03eeb93f1b2b412202fd4c415eb9
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-23 19:10:04 +0530

    Update Last_index_of_element_in_array.cpp

--- Recursion/Last_index_of_element_in_array.cpp
+++ Recursion/Last_index_of_element_in_array.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int lastIndex(int arr[],int n,int x){
   if(n==0){
-    return -1;
+    return -1; // if size of array is 0 then we return -1
   }
-  int ans=lastIndex(arr+1,n-1,x);
-  if(ans==-1){
-  if(arr[0]==x){
+  int ans=lastIndex(arr+1,n-1,x);//recursion call to the next element of array.
+  if(ans==-1){ //if we cannot find the element in the remaining part of array 
+  if(arr[0]==x){//We check for 1st element.
     return 0;
   }
   else{
-    return -1;
+    return -1;//else we return -1
   }
   }
   
