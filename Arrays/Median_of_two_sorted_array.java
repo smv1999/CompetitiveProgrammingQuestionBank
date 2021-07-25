e2f15f85f82776bd09f506794c053f4bd1481dde
Rajesh Kumar <80545227+rajeshkumar2024@users.noreply.github.com>
2021-07-25 09:57:54 +0530

    Update Median_of_two_sorted_array.java

--- Arrays/Median_of_two_sorted_array.java
+++ Arrays/Median_of_two_sorted_array.java
@@ -1,5 +1,6 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //Creating a ArrayList containing all the elements of both array
         List<Integer> merged = new ArrayList<Integer>();
         for (int i= 0 ; i < nums1.length; i++){
             merged.add(nums1[i]);
@@ -8,10 +9,14 @@ class Solution {
             merged.add(nums2[i]);
         }
         Collections.sort(merged);
+        //Sorting the finally Merged Linked List
         int idx = merged.size()/2;
+        //Finding the middle index to get the Median
         if (merged.size()%2 != 0){
+            //If Size of arraylist is odd then the middle element will be the median of the ArrayList.
             return merged.get(idx);
         }
+        
         return (merged.get(idx-1)+merged.get(idx))/2.0;
 
     }
