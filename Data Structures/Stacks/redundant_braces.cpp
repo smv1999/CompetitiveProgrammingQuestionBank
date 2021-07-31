872353b7b85d18189195a2e9ff4deb76e09b1804
ishikasinha-d <69107978+ishikasinha-d@users.noreply.github.com>
2021-07-31 22:21:45 +0530

    Update redundant_braces.cpp

--- Data Structures/Stacks/redundant_braces.cpp
+++ Data Structures/Stacks/redundant_braces.cpp
@@ -11,20 +11,26 @@ int redundantBraces(string A) {
     
     for(int i=0; i< n; i++)
     {
+        //we'll push the operators and opening braces in the stack
         if(A[i]=='+' || A[i]=='-'|| A[i]=='/'|| A[i]=='*' || A[i]=='(' )
             s.push(A[i]);
-            
+        
+     //when we encounter closing braces, we need to start popping
         if(A[i]==')')
         { 
+         //if there are no operators, it means we have redundant braces for eg. ()
             if(s.top()=='(')
                 return 1;
- 
+         
+         //else we start popping till we encounter opening braces
+         //count variable will keep count of number of operators enclosed within the braces
             int count=0;
             while(!s.empty() && s.top()!='(')
             {
                 s.pop();
                 count++;
             }
+         //count=0 means we have something like (a) which has redundant braces
             if(count==0) 
                 return 1;
             s.pop();
