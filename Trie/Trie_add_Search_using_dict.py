4fae07154abb7e3b0ec4ce403074c12b0b74f3a2
Prashant raj <75080313+PrashantVIT1@users.noreply.github.com>
2021-08-04 22:37:11 +0530

    Add files via upload

---
+++ Trie/Trie_add_Search_using_dict.py
@@ -0,0 +1,32 @@
+class Trie:
+    root = {}
+    
+    def add(self, word):
+        cur = self.root
+        
+        for ch in word:
+            if ch not in cur:
+                cur[ch] = {}
+            cur = cur[ch]
+        cur['*'] = True
+        
+    def search(self,word):
+        cur = self.root
+        
+        for ch in word:
+            if ch not in cur:
+                return False
+            cur = cur[ch]
+        if '*' in cur:
+            return True
+        else:
+            return False
+dictionary = Trie()
+dictionary.add("pqr")
+dictionary.add("pqrst")
+print(dictionary.search("pqr"))
+print(dictionary.search("pq"))
+print(dictionary.search("p"))
+print(dictionary.search("pqrst"))
+dictionary.add("p")
+print(dictionary.search("p"))
