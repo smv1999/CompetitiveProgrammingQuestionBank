41367d80613ca5089ab35256c8b2a88f875997f7
Prashant raj <75080313+PrashantVIT1@users.noreply.github.com>
2021-08-05 21:18:12 +0530

    dynamic input and comments done

--- Trie/Trie_add_Search_using_dict.py
+++ Trie/Trie_add_Search_using_dict.py
@@ -1,33 +1,51 @@
-"""Trie DS implementation in Python"""
+"""
+Inputs:
+1nd line no of word you want to store in Trie (int)
+2rd line to (2+1st line input)th line (string)
+Next line enter number of words want to search
+then enter strings in each line
+
+"""
 class Trie:
-    root = {}
+    root = {} #main dictionary of Trie
     
-    def add(self, word):
-        cur = self.root
+    def add(self, word): # This funtion adds new word to Trie
+        cur = self.root  # cur is iterator to check whether new key/paths are required to add new word and root is main dictionary
         
-        for ch in word:
-            if ch not in cur:
-                cur[ch] = {}
-            cur = cur[ch]
-        cur['*'] = True
+        for ch in word: # ch is all the charactors in the word 
+            if ch not in cur: # if charactor is not present add new key/path initiate it empty
+                cur[ch] = {}  
+            cur = cur[ch]    # now go to the key that was created or was present
+        cur['*'] = True   # set * key to true at the end of each word
+        #so larger the word the dictionary nesting will take place and * key represant the end state of the word
         
-    def search(self,word):
-        cur = self.root
+    def search(self,word): # This function searches whether the word is presant or not presant
+        cur = self.root  #cur is iterator to check whether new key/paths are presant or not
         
-        for ch in word:
-            if ch not in cur:
+        for ch in word:    # ch is all the charactors in the word 
+            if ch not in cur:  # if ch is not found in cur that means word not presant so result is false
                 return False
-            cur = cur[ch]
-        if '*' in cur:
+            cur = cur[ch] #if ch present then go till the end unless it becomes false
+        if '*' in cur: # if * key not present that means words is prefix of a word that is present but word is not Present
             return True
         else:
             return False
+# dynamic input 
+n2 = int(input("No of words to be Added:\n")) 
+print("enter words:")
+words = []
+checks = []
+for j in range(n2):
+    words.append(input())
+n3 = int(input("No of words to be searched:\n"))
+print("enter words:")
+for j in range(n3):
+    checks.append(input())
+
+        
 dictionary = Trie()
-dictionary.add("pqr")
-dictionary.add("pqrst")
-print(dictionary.search("pqr"))
-print(dictionary.search("pq"))
-print(dictionary.search("p"))
-print(dictionary.search("pqrst"))
-dictionary.add("p")
-print(dictionary.search("p"))
+
+for k in words:
+    dictionary.add(k)
+for k in checks:
+    print(dictionary.search(k))
