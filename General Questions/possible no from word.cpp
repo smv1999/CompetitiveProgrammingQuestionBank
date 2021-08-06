e6a0cd6b1ef8347e8c47c7461b033776636f8c4e
Harsh kumar <1905391@kiit.ac.in>
2021-08-06 12:21:31 +0530

    Update possible no from word.cpp

--- General Questions/possible no from word.cpp
+++ General Questions/possible no from word.cpp
@@ -1,118 +1,142 @@
 /* C program to print a given number in words. The program
 handles numbers from 0 to 9999 
 Just take an example  if the user privide the number
-1234 -Input
-One thousand Two hundred and twenty four -Output
+INPUT-
+1-No of test cases
+1234-the given number
+OUTPUT- 
+One thousand Two hundred and twenty four */
+
+#include <bits/stdc++.h>
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             LONG_LONG_MAX
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define endl            '\n'
+#define mk(arr,n,type)  type *arr=new type[n]
+#define w(x)            int x; cin >> x; while(x--)
+#define f(i,x,y)        for(int i = x; i < y; i++)
+#define g(i,x,y)        for(int i = x;i<=y;i++)
 
 
 
+using namespace std;
+string one[] = { "", "one ", "two ", "three ", "four ",
+        "five ", "six ", "seven ", "eight ",
+        "nine ", "ten ", "eleven ", "twelve ",
+        "thirteen ", "fourteen ", "fifteen ",
+        "sixteen ", "seventeen ", "eighteen ",
+        "nineteen " };
 
-*/
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// strings at index 0 and 1 are not used, they is to
+// make array indexing simple
+string ten[] = { "", "", "twenty ", "thirty ", "forty ",
+        "fifty ", "sixty ", "seventy ", "eighty ",
+        "ninety " };
 
-/* A function that prints given number in words */
-void convert_to_words(char* num)
+
+
+void c_p_p()
 {
-	int len = strlen(
-		num); // Get number of digits in given number
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	/* Base cases */
-	if (len == 0) {
-		fprintf(stderr, "empty string\n");
-		return;
-	}
-	if (len > 4) {
-		fprintf(stderr,
-				"Length more than 4 is not supported\n");
-		return;
-	}
+}
+/* ******* Your Functions Below ******** */
 
-	/* The first string is not used, it is to make
-		array indexing simple */
-	char* single_digits[]
-		= { "zero", "one", "two", "three", "four",
-			"five", "six", "seven", "eight", "nine" };
 
-	/* The first string is not used, it is to make
-		array indexing simple */
-	char* two_digits[]
-		= { "",		 "ten",	 "eleven", "twelve",
-			"thirteen", "fourteen", "fifteen", "sixteen",
-			"seventeen", "eighteen", "nineteen" };
+// n is 1- or 2-digit number
+string numToWords(int n, string s)
+{
+  string str = "";
+  // if n is more than 19, divide it
+  if (n > 19)
+    str += ten[n / 10] + one[n % 10];
+  else
+    str += one[n];
 
-	/* The first two string are not used, they are to make
-		array indexing simple*/
-	char* tens_multiple[] = { "",	 "",	 "twenty",
-							"thirty", "forty", "fifty",
-							"sixty", "seventy", "eighty",
-							"ninety" };
+  // if n is non-zero
+  if (n)
+    str += s;
 
-	char* tens_power[] = { "hundred", "thousand" };
-
-	/* Used for debugging purpose only */
-	printf("\n%s: ", num);
-
-	/* For single digit number */
-	if (len == 1) {
-		printf("%s\n", single_digits[*num - '0']);
-		return;
-	}
-
-	/* Iterate while num is not '\0' */
-	while (*num != '\0') {
-
-		/* Code path for first 2 digits */
-		if (len >= 3) {
-			if (*num - '0' != 0) {
-				printf("%s ", single_digits[*num - '0']);
-				printf("%s ",
-					tens_power[len - 3]); // here len can
-											// be 3 or 4
-			}
-			--len;
-		}
-
-		/* Code path for last 2 digits */
-		else {
-			/* Need to explicitly handle 10-19. Sum of the
-			two digits is used as index of "two_digits"
-			array of strings */
-			if (*num == '1') {
-				int sum = *num - '0' + *(num + 1) - '0';
-				printf("%s\n", two_digits[sum]);
-				return;
-			}
-
-			/* Need to explicitely handle 20 */
-			else if (*num == '2' && *(num + 1) == '0') {
-				printf("twenty\n");
-				return;
-			}
-
-			/* Rest of the two digit numbers i.e., 21 to 99
-			*/
-			else {
-				int i = *num - '0';
-				printf("%s ", i ? tens_multiple[i] : "");
-				++num;
-				if (*num != '0')
-					printf("%s ",
-						single_digits[*num - '0']);
-			}
-		}
-		++num;
-	}
+  return str;
 }
 
-/* Driver program to test above function */
-int main(void)
+// Function to print a given number in words
+string convertToWords(long n)
 {
-	convert_to_words("9923");
-	convert_to_words("523");
-	convert_to_words("89");
-	convert_to_words("8");
+  // stores word representation of given number n
+  string out;
 
-	return 0;
+  // handles digits at ten millions and hundred
+  // millions places (if any)
+  out += numToWords((n / 10000000), "crore ");
+
+  // handles digits at hundred thousands and one
+  // millions places (if any)
+  out += numToWords(((n / 100000) % 100), "lakh ");
+
+  // handles digits at thousands and tens thousands
+  // places (if any)
+  out += numToWords(((n / 1000) % 100), "thousand ");
+
+  // handles digit at hundreds places (if any)
+  out += numToWords(((n / 100) % 10), "hundred ");
+
+  if (n > 100 && n % 100)
+    out += "and ";
+
+  // handles digits at ones and tens places (if any)
+  out += numToWords((n % 100), "");
+
+  return out;
 }
+
+/* ******* Your Functions Above ******** */
+
+int32_t main()
+{
+	c_p_p();
+	#ifndef ONLINE_JUDGE
+	// For getting input from input.txt file 
+    // For getting input from input.txt file 
+    freopen("inpute.exe", "r", stdin); 
+  
+    // Printing the Output to output.txt file 
+    freopen("output.exe", "w", stdout); 
+#endif
+    int t;
+    cin>>t;
+    
+    while(t--)
+{
+    long n ;
+  cin>>n;
+
+  // convert given number in words
+  cout << convertToWords(n) << endl;
+   
+
+}
+return 0;
+}
+/*Time complexity:
+Time complexity: O(1). 
+The loop runs for a constant amount of time.
+Auxiliary space: O(1). 
+As no extra space is required.
+Time complexity: O(1). */
+
+
+	
+	
