fdb9767d4a4e8bbc7bc5ec4f864b2c6e1618c497
Suraj Vatsya <surajkumar742494@gmail.com>
2021-08-10 20:39:51 +0530

    Update smallest_next_palindrome.cpp
    added comments for applied logic

--- Numbers/Basic_Programs/smallest_next_palindrome.cpp
+++ Numbers/Basic_Programs/smallest_next_palindrome.cpp
@@ -1,4 +1,8 @@
+//We have given an integer N. We have to find out the smallest next palindrome of that integer value.
+//suppose we have given 113 as a number so the next smallest palindrome number is 121.
 
+// Intution :- Take a loop from N+1 to INFINITE and call a function ispalindrome which will check wheather the number is palindrome or not.
+//In isPalindrome function we will convert the integer into string and then reverse that string , compare original string to reversed one and if both are same return true.
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -7,12 +11,12 @@ using namespace std;
 int mx = INT_MAX;
 bool isPalindrome(int i)
 {
-  string s1 = "";
-  string s = to_string(i);
-  s1.append(s);
-  reverse(s1.begin(), s1.end());
-  if (s.compare(s1) == 0)
-    return true;
+  string s1 = "";                    //take a empty string to copy original string
+  string s = to_string(i);          //convert integer to string
+  s1.append(s);                    //append  original string to s1.
+  reverse(s1.begin(), s1.end());    //reverse copied string
+  if (s.compare(s1) == 0)           //comparing both string
+    return true;                    //if both are equal return true else return false
   return false;
 }
 int main()
