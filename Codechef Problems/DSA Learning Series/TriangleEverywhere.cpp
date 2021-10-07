25f50505ae5fbc3e9794145043f9d54b356178f0
Utkarsh Mathur <78642104+MathurUtkarsh@users.noreply.github.com>
2021-10-07 21:55:42 +0530

    Update TriangleEverywhere.cpp

--- Codechef Problems/DSA Learning Series/TriangleEverywhere.cpp
+++ Codechef Problems/DSA Learning Series/TriangleEverywhere.cpp
@@ -1,20 +1,25 @@
+/* You're given the length of three sides a, b, and c respectively. 
+Now If these three sides can form an Equilateral Triangle then print 1, if these three sides can form an Isosceles Triangle then print 2, 
+if these three sides can form a Scalene Triangle then print 3, otherwise print −1.*/
+
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int a, b, c;
-    cin >> a >> b >> c;
-    if (a + b > c && b + c > a && c + a > b)
+    int a, b, c; 
+    cin >> a >> b >> c; 
+    if (a + b > c && b + c > a && c + a > b) // condition for a triangle.
     {
-        if (a == b && b == c)
+        if (a == b && b == c) // condition for equilateral triangle.
             cout << "1";
-        else if (a == b || b == c || c == a)
+        else if (a == b || b == c || c == a) // condition for isosceles triangle.
             cout << "2";
         else
-            cout << "3";
+            cout << "3"; 
     }
     else
-        cout << "-1";
+        cout << "-1"; 
     return 0;
 }
