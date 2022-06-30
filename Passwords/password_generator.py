cbb47d9d68719a9beda15f554ca2e387bf6a57cb
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-06-30 17:29:55 +0530

    Update password_generator.py

--- Passwords/password_generator.py
+++ Passwords/password_generator.py
@@ -1,1 +1,16 @@
+#generating hard and random password
 
+import random
+
+lower_char="abcdefghijklmnopqrstuvwxyz"
+upper_char="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number="1234567890"
+special_char="!@#$%^&*"
+
+total= lower_char+upper_char+number+special_char
+
+length_of_password=10
+
+password="".join(random.sample(total,length_of_password))
+
+print("your genereted password is",password)
